c2b617c8d6110f588adefebb57a1a8baa73c7b68
Zabloon <zabloona@gmail.com>
2023-11-16 09:14:33 +0500

    added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ limit: "200mb" }));
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(require("./routes/invoice"));
 app.use(require("./routes/proformaInvoice"));
